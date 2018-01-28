16940fa3eb5c170db5b0fccdc10d87b61e0e55fd
dacoolkid <zakkoenigs@gmail.com>
2018-01-28 15:58:46 -0800

    Add files via upload

---
+++ LeagueInvaders-master/src/GameObject.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics;
+
+public class GameObject {
+    int x;
+
+    int y;
+
+    int width;
+
+    int height;
+    public GameObject(int x, int y, int width, int height) {
+    	
+    	
+    }
+    public void update() {
+    	
+    }
+    
+    
+    public void draw(Graphics g){
+    	g.fillRect(90, 50, 100, 100);
+	                
+
+	        }
+    	
+    	
+    }
+
