a546b36d8c49b86971fbf288fe55c8220ac38f66
dacoolkid <zakkoenigs@gmail.com>
2018-02-25 15:57:23 -0800

    YesNoMaybe

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,10 +10,37 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{  
- 
+	final int MENU_STATE = 0;
+
+	final int GAME_STATE = 1;
+
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
+	
+	public Font titleFont;
+	public GamePanel() {
+		 titleFont = new Font("Arial",Font.PLAIN, 48);
+		 
+	}
+	
 	@Override
+	
 
 	public void paintComponent(Graphics g){
+		if(currentState == MENU_STATE){
+			System.out.println("We are missing something here we gotta figure out what it is");
+            drawMenuState(g);
+
+    }else if(currentState == GAME_STATE){
+
+            drawGameState(g);
+
+    }else if(currentState == END_STATE){
+
+            drawEndState(g);
+
+    }
+
 
 	                
 
@@ -22,6 +51,20 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+	
+		if(currentState == MENU_STATE){
+		
+            updateMenuState();
+
+    }else if(currentState == GAME_STATE){
+
+            updateGameState();
+
+    }else if(currentState == END_STATE){
+
+            updateEndState();
+
+    }
 		repaint();
 	}
 	
@@ -33,18 +76,55 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("gay");
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("supergay");
-	}
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+currentState++;
+if(currentState > END_STATE){
+	
 
+                currentState = MENU_STATE;
+
+        }
+		// TODO Auto-generated method stub
+	
+	}
+	}
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("megagay");
+	
 	}
+	public void updateMenuState() {
+		
+	}
+public void updateGameState() {
+		
+	}
+public void updateEndState() {
+	
 }
+	public void drawMenuState(Graphics g) {
+		g.setColor(Color.PINK);
+		g.setFont(titleFont);
+		   g.drawString("text", 90, 90);
+		   
+		g.setColor(Color.BLUE);
+
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);    
+	}
+	public void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);    
+	}
+	public void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);    
+	}
+	}
+
